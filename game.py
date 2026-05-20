128caad26d62239488bb1f79934402dffa5d1460
AIexMa <a@b.com>
2026-05-20 22:06:43 +0000

    Wed, May 20, 2026, 3:06 PM -07:00

--- game.py
+++ game.py
@@ -1020,8 +1020,8 @@ class TutorialGuide:
             max_bubble_width / TUTORIAL_GUIDE_BUBBLE_ASPECT_RATIO,
         )
         bubble_width = bubble_height * TUTORIAL_GUIDE_BUBBLE_ASPECT_RATIO
-        bubble_center_x = layout.width - layout.sx(6) - bubble_width / 2 - layout.sx(28)
-        bubble_center_y = layout.sy(-52) + bubble_height / 2
+        bubble_center_x = layout.width - layout.sx(6) - bubble_width / 2 - layout.sx(18)
+        bubble_center_y = layout.sy(-72) + bubble_height / 2
         sprite_size = min(layout.ss(360), max(layout.ss(230), min(layout.width, layout.height) * 0.42))
         sprite_center_x = layout.width - layout.sx(116)
         sprite_center_y = 0.0
